c83ea53b9927bcedb5c43807415ea2cd1156a9b5
Branislav Remen <branislavremen@outlook.com>
2017-02-07 20:20:37 +0100

    api save/update/delete todo

--- routes/todos.js
+++ routes/todos.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 //setup mongo database
 var mongojs = require('mongojs');
-var db = mongojs('mongodb://brano:brano@ds145659.mlab.com:45659/meantodos', ['todos']); //uri, collection from db
+var db = mongojs('mongodb://<username>:<pwd>@ds145659.mlab.com:45659/meantodos', ['todos']); //uri, collection from db
 
 //get collection Todos from db
 //can be called with localhost:3000/api/v1/todos
@@ -29,6 +29,70 @@ router.get('/todo/:id', function (req, res, next) {
             res.json(todos);
         }
     })
+});
+
+// save todo
+// POST on localhost:3000/api/v1/todo
+router.post('/todo', function (req, res, next) {
+   var todo = req.body;
+   if(!todo.text || !(todo.isCompleted + '')){
+       res.status(400);
+       res.json({
+           "error": "Invalid Data"
+       });
+   } else {
+       db.save(todo, function (err, result) {
+           if(err){
+               res.send(err);
+           } else {
+               res.json(result);
+           }
+       })
+   }
+});
+
+//update todo api
+router.put('/todo/:id', function (req, res, next) {
+    var todo = req.body;
+    var updObj = {};
+
+    if(todo.isCompleted){
+        updObj.isCompleted = todo.isCompleted;
+    }
+
+    if(todo.text){
+        updObj.text = todo.text;
+    }
+
+    if(!updObj){
+        res.status(400);
+        res.json({
+            "error": "Invalid Data"
+        });
+    } else {
+        db.todos.update({
+            _id: mongojs.ObjectId(req.params.id)
+        }, updObj, {}, function (err, result) {
+            if(err){
+                res.send(err);
+            } else {
+                res.json(result);
+            }
+        });
+    }
+});
+
+//delete todo API
+router.delete('/todo/:id', function (req, res, next) {
+    db.todos.remove({
+        _id: mongojs.ObjectId(req.params.id)
+    }, '', function (err, result) {
+        if(err){
+            res.send(err);
+        } else {
+            res.json(result);
+        }
+    });
 });
 
 module.exports = router;
