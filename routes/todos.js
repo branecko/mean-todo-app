f80ede7bf8efee45f8b1c4defdb88ab37cac354c
Branislav Remen <branislavremen@outlook.com>
2017-02-07 19:44:16 +0100

    setup mongodb (mlab)

--- routes/todos.js
+++ routes/todos.js
@@ -1,8 +1,19 @@
 var express = require('express');
 var router = express.Router();
 
+//setup mongo database
+var mongojs = require('mongojs');
+var db = mongojs('mongodb://<dbuser>:<dbpassword>@ds145659.mlab.com:45659/meantodos', ['todos']); //uri, collection from db
+
+//get collection from db
 router.get('/todos', function (req, res, next) {
-    res.send('TODOS API');
+    db.todos.find(function (err, todos) {
+        if(err){
+          res.send(err);
+        } else {
+          res.json(todos);
+        }
+    })
 });
 
 module.exports = router;
